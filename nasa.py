a7f4b6cd86f4486a05bffeacae78526dd8620ba9
Soleil <germinaaaaal@gmail.com>
2021-01-17 20:28:11 +0100

    build fetching algorithm

--- nasa.py
+++ nasa.py
@@ -2,31 +2,46 @@ import datetime
 import urllib3
 from bs4 import BeautifulSoup
 
-# Get current time
-now = datetime.datetime.now()
-date = "{}-{}-{}".format(now.year, now.month, now.day)
-print(date)
+# Link planets to their Horizons code
+planet_codes = {
+    "Sun" : 10,
+    "Mercury" : 199,
+    "Venus" : 299,
+    "Mars" : 499,
+    "Jupiter" : 599,
+    "Saturn" : 699,
+    "Uranus" : 799,
+    "Neptune" : 899,
+    "Pluto" : 999
+}
 
-# Curate JPL-Horizons request
+# This will be to get more precise placements
+"""
+now = datetime.datetime.now()
+
+date = "{}-{}-{} {}:{}".format(now.year, now.month, now.day, now.hour, now.minute)
+print(now.isoformat())
+
+nowtomorrow = datetime.datetime.now() + datetime.timedelta(days=1)
+print("now.tomorrow: ", nowtomorrow.isoformat())
+"""
+# Get current date
+date = datetime.date.today.isoformat()
 
 # Fetch the html file
 http = urllib3.PoolManager()
-r = http.request("GET", "https://ssd.jpl.nasa.gov/horizons_batch.cgi?batch=1&COMMAND='499'&MAKE_EPHEM='YES'&TABLE_TYPE='OBSERVER'&START_TIME='2000-01-01'&STOP_TIME='2000-12-31'&STEP_SIZE='15%20d'&QUANTITIES='1,9,20,23,24'&CSV_FORMAT='YES'")
 
-batch = r.data.decode("utf-8").split("\n")
+for planet in planet_codes:
+    r = http.request("GET", "https://ssd.jpl.nasa.gov/horizons_batch.cgi?batch=\
+    1&COMMAND='{0}'&MAKE_EPHEM='YES'&TABLE_TYPE='OBSERVER'&START_TIME='{1} 00:0\
+    0'&STOP_TIME='{1} 23:59'&STEP_SIZE='3h'&QUANTITIES='31'&CSV_FORMAT='YES'".\
+    format(planet_codes[planet], date))
 
-start = batch.index("$$SOE")
-end = batch.index("$$EOE")
+    batch = r.data.decode("utf-8").split("\n")
+    print(batch)
+    start = batch.index("$$SOE")
+    end = batch.index("$$EOE")
 
-data = batch[start+1:end]
-for line in data:
-    print(line)
-
-# Parse the html file
-#soup = BeautifulSoup(html_doc, 'html.parser')
-
-# Format the parsed html file
-#strhtm = soup.prettify()
-
-# Print the first few characters
-#print (strhtm[:225])
+    data = batch[start+1:end]
+    for line in data:
+        print(planet + " " + line)
